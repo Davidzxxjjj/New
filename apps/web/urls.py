576eee28b0e7a2f12ecd4dd93630af4b93f1d5bc
David <davidzxxjjj2003@gmail.com>
2021-09-28 22:42:39 -0600

    Change deboug

---
+++ apps/web/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('mascota', mascota, name='mascota'),
+    path('dueños', dueños, name='dueños'),
+    path('citas', citas, name='citas'),
+]
